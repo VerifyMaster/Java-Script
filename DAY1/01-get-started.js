08122192da5c6c9429b5a879d64e47c6305161e9
VerifyMaster <gauravbhardwaj940@gmail.com>
2026-08-15 23:29:31 +0530

    Update 01-get-started.js

--- DAY1/01-get-started.js
+++ DAY1/01-get-started.js
@@ -1,1 +1,11 @@
 
+var x // declaration =>create memory space to hold a value
+
+// Double slash => keyboard shortcut => ctrl+/ => is used to comment a single line
+
+/* TO comment
+
+a paragraph we use
+
+keyboard shortcut
+alt+shift+a */
